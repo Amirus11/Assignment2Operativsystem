b5d8b8226e00f4c95a4b29516dea16e097d1db4d
Elias-Chairi <eliaschairi@icloud.com>
2026-03-11 13:02:25 +0100

    refactored Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * The {@link Test} class represents a test suite for the process scheduling algorithms. It defines
@@ -10,66 +11,33 @@ import java.util.function.Function;
  * @see Scheduler
  */
 public class Test {
-  private static List<Job> getJobsCase1() {
-    return List.of(
-        new Job(0, new Process("p1", 8)),
-        new Job(0, new Process("p2", 4)),
-        new Job(0, new Process("p3", 2)),
-        new Job(0, new Process("p4", 6)),
-        new Job(0, new Process("p5", 3)));
-  }
-
-  private static List<Job> getJobsCase2() {
-    return List.of(
-        new Job(0, new Process("p1", 20)),
-        new Job(1, new Process("p2", 2)),
-        new Job(2, new Process("p3", 2)),
-        new Job(3, new Process("p4", 1)),
-        new Job(4, new Process("p5", 3)));
-  }
-
-  private static List<Job> getJobsCase3() {
-    return List.of(
-        new Job(0, new Process("p1", 20)),
-        new Job(1, new Process("p2", 2)),
-        new Job(2, new Process("p3", 2)),
-        new Job(3, new Process("p4", 2)),
-        new Job(4, new Process("p5", 2)),
-        new Job(5, new Process("p6", 2)));
-  }
+  private static List<Supplier<List<Job>>> testCases =
+      List.of(
+          () ->
+              List.of(
+                  new Job(0, new Process("p1", 8)),
+                  new Job(0, new Process("p2", 4)),
+                  new Job(0, new Process("p3", 2)),
+                  new Job(0, new Process("p4", 6)),
+                  new Job(0, new Process("p5", 3))),
+          () ->
+              List.of(
+                  new Job(0, new Process("p1", 20)),
+                  new Job(1, new Process("p2", 2)),
+                  new Job(2, new Process("p3", 2)),
+                  new Job(3, new Process("p4", 1)),
+                  new Job(4, new Process("p5", 3))),
+          () ->
+              List.of(
+                  new Job(0, new Process("p1", 20)),
+                  new Job(1, new Process("p2", 2)),
+                  new Job(2, new Process("p3", 2)),
+                  new Job(3, new Process("p4", 2)),
+                  new Job(4, new Process("p5", 2)),
+                  new Job(5, new Process("p6", 2))));
 
   private static final String blue = "\u001B[34m";
   private static final String reset = "\u001B[0m";
-
-  private Simulation[] simFCFS = {
-    new Simulation(getJobsCase1(), new FirstComeFirstServe()),
-    new Simulation(getJobsCase2(), new FirstComeFirstServe()),
-    new Simulation(getJobsCase3(), new FirstComeFirstServe())
-  };
-
-  private Simulation[] simSJF = {
-    new Simulation(getJobsCase1(), new ShortestJobFirst()),
-    new Simulation(getJobsCase2(), new ShortestJobFirst()),
-    new Simulation(getJobsCase3(), new ShortestJobFirst())
-  };
-
-  private Simulation[] simSRTF = {
-    new Simulation(getJobsCase1(), new ShortestRemainingTimeFirst()),
-    new Simulation(getJobsCase2(), new ShortestRemainingTimeFirst()),
-    new Simulation(getJobsCase3(), new ShortestRemainingTimeFirst())
-  };
-
-  /**
-   * Runs the given simulations by calling the {@link Simulation#run()} method on each simulation in
-   * the array.
-   *
-   * @param sims the array of simulations to be run
-   */
-  private static void runSimulations(Simulation[] sims) {
-    for (int i = 0; i < sims.length; i++) {
-      sims[i].run();
-    }
-  }
 
   /**
    * Prints the statistics for the given simulations.
@@ -113,17 +81,35 @@ public class Test {
    *     turnaround time
    */
   void main(boolean printStats, boolean printTable) {
-    runSimulations(simFCFS);
-    runSimulations(simSJF);
-    runSimulations(simSRTF);
+    var simFCFS = new Simulation[testCases.size()];
+    var simSJF = new Simulation[testCases.size()];
+    var simSRTF = new Simulation[testCases.size()];
+
+    for (int i = 0; i < testCases.size(); i++) {
+      var testCase = testCases.get(i);
+      simFCFS[i] = new Simulation(testCase.get(), new FirstComeFirstServe());
+      simFCFS[i].run();
+      simSJF[i] = new Simulation(testCase.get(), new ShortestJobFirst());
+      simSJF[i].run();
+      simSRTF[i] = new Simulation(testCase.get(), new ShortestRemainingTimeFirst());
+      simSRTF[i].run();
+    }
+
     if (printStats) {
       printSimulationStats(simFCFS, "First Come First Serve");
       printSimulationStats(simSJF, "Shortest Job First");
       printSimulationStats(simSRTF, "Shortest Remaining Time");
     }
+
     if (printTable) {
-      printCompTable("Average waiting time", Simulation::getAverageWaitingTime);
-      printCompTable("Average turnaround time", Simulation::getAverageTurnaroundTime);
+      printCompTable(
+          "Average waiting time", Simulation::getAverageWaitingTime, simFCFS, simSJF, simSRTF);
+      printCompTable(
+          "Average turnaround time",
+          Simulation::getAverageTurnaroundTime,
+          simFCFS,
+          simSJF,
+          simSRTF);
     }
   }
 
@@ -147,7 +133,12 @@ public class Test {
    * @param func the function to be applied to each simulation to get the value to be printed in the
    *     table
    */
-  private void printCompTable(String title, Function<Simulation, Double> func) {
+  private void printCompTable(
+      String title,
+      Function<Simulation, Double> func,
+      Simulation[] sim1,
+      Simulation[] sim2,
+      Simulation[] sim3) {
     StringBuilder sb1 = new StringBuilder();
     sb1.append(blue + "============" + title + "============" + reset + "\n");
     sb1.append("Cases | FCFS | SJF  | SRTF | \n");
@@ -156,11 +147,11 @@ public class Test {
       moveCursorToCol(1);
       IO.print((i + 1) + "     | ");
       moveCursorToCol(9);
-      IO.print(func.apply(simFCFS[i]));
+      IO.print(func.apply(sim1[i]));
       moveCursorToCol(14);
-      IO.print("| " + func.apply(simSJF[i]));
+      IO.print("| " + func.apply(sim2[i]));
       moveCursorToCol(21);
-      IO.print("| " + func.apply(simSRTF[i]));
+      IO.print("| " + func.apply(sim3[i]));
       moveCursorToCol(28);
       IO.println("|");
     }
